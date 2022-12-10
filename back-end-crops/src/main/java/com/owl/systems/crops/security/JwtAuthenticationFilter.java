aa95888c26ace3431031f18a65b106411759f8b1
Felipe D. Santos <lycan.felipe@gmail.com>
2022-12-10 12:40:59 -0300

    Back-End: Jwt Authentification Filter

--- back-end-crops/src/main/java/com/owl/systems/crops/security/JwtAuthenticationFilter.java
+++ back-end-crops/src/main/java/com/owl/systems/crops/security/JwtAuthenticationFilter.java
@@ -1,11 +1,43 @@
 package com.owl.systems.crops.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-//    @Autowired
-//    private JwtPr
+    @Autowired
+    private JwtProvider jwtProvider;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, jakarta.servlet.ServletException {
+        String jwt = getJwtFromRequest(request);
+        if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
+            String userName = jwtProvider.getUserNameFromJwt(jwt);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            authenticationToken.setDetails(request);
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return bearerToken;
+    }
 }
