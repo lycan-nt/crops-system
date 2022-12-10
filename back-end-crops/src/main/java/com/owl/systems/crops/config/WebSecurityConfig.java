e9164c328543de5e57206989a487172169f5fe94
Felipe D. Santos <lycan.felipe@gmail.com>
2022-12-10 13:16:23 -0300

    Back-End: Implemented WebSecurityConfig

--- back-end-crops/src/main/java/com/owl/systems/crops/config/WebSecurityConfig.java
+++ back-end-crops/src/main/java/com/owl/systems/crops/config/WebSecurityConfig.java
@@ -1,10 +1,19 @@
 package com.owl.systems.crops.config;
 
+import com.owl.systems.crops.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -13,7 +22,35 @@ public class WebSecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
- //   @Bean
-//    public JwtAuthenticationFilter
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**")
+                .permitAll()
+                .requestMatchers("/api/posts/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        var dao = new DaoAuthenticationProvider();
+        dao.setUserDetailsService(userDetailsService);
+        dao.setPasswordEncoder(passwordEnconder());
+        return new ProviderManager(dao);
+    }
+
+    private PasswordEncoder passwordEnconder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
