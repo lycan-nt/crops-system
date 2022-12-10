13d779971f8e7f149bb53a82117b205fa8295e96
Felipe D. Santos <lycan.felipe@gmail.com>
2022-12-10 11:31:06 -0300

    BackEnd: Implemented JwtProvider

--- back-end-crops/src/main/java/com/owl/systems/crops/security/JwtProvider.java
+++ back-end-crops/src/main/java/com/owl/systems/crops/security/JwtProvider.java
@@ -1,25 +1,74 @@
 package com.owl.systems.crops.security;
 
+import com.owl.systems.crops.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.annotation.PostConstruct;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Value;
+import java.io.IOException;
 import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
 public class JwtProvider {
 
     private KeyStore keyStore;
+    @Value("${jks.pass}")
+    private String jksPass;
 
     @PostConstruct
     public void init() {
         try {
             this.keyStore = KeyStore.getInstance("JKS");
-            InputStream resourceAsStream = getClass().getResourceAsStream("");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/crops.jks");
+            this.keyStore.load(resourceAsStream, this.jksPass.toCharArray());
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(user.getUserName())
+                .signWith(getPrivateKey())
+                .compact();
+    }
+
+    private Key getPrivateKey() {
+        try {
+            return (PrivateKey) this.keyStore.getKey("crops", this.jksPass.toCharArray());
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (UnrecoverableKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean validateToken(String jwt) {
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJwt(jwt);
+        return true;
+    }
+
+    private PublicKey getPublicKey() {
+        try {
+            return this.keyStore.getCertificate("crops").getPublicKey();
         } catch (KeyStoreException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String getUserNameFromJwt(String jwt) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(getPublicKey())
+                .parseClaimsJwt(jwt)
+                .getBody();
+        return claims.getSubject();
     }
 
 }
