b3a55c5e711bf5268d8a4d49bec849af3991db8a
Felipe D. Santos <lycan.felipe@gmail.com>
2022-12-29 11:41:40 -0300

    Test resource findAll in EventController

--- back-end-crops/src/main/java/com/owl/systems/crops/controller/EventController.java
+++ back-end-crops/src/main/java/com/owl/systems/crops/controller/EventController.java
@@ -1,15 +1,25 @@
 package com.owl.systems.crops.controller;
 
+import com.owl.systems.crops.model.Event;
+import com.owl.systems.crops.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/events")
 public class EventController {
 
+    @Autowired
+    private EventService eventService;
+
     @GetMapping
-    @ResponseBody
-    public String findAll() {
-        return "OK";
+    public ResponseEntity<List<Event>> findAll() {
+        List<Event> eventList = this.eventService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(eventList);
     }
 
 }
--- back-end-crops/src/test/java/com/owl/systems/crops/controller/EventControllerTest.java
+++ back-end-crops/src/test/java/com/owl/systems/crops/controller/EventControllerTest.java
@@ -1,9 +1,14 @@
 package com.owl.systems.crops.controller;
 
+import com.owl.systems.crops.repository.EventRepository;
+import com.owl.systems.crops.service.EventService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,8 +19,20 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @WebMvcTest(EventController.class)
 public class EventControllerTest {
 
+    @TestConfiguration
+    static class EventControllerTestConfiguration {
+        @Bean
+        public EventService eventService() {
+            return new EventService();
+        }
+    }
+
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private EventService eventService;
+    @MockBean
+    private EventRepository eventRepository;
 
     @Test
     @WithMockUser(username="tdd",roles={"ADMIN"})
