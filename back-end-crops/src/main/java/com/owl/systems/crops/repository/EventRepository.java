30e3cb7d35ac90d4fd646486d2e1703e0b3db767
Felipe D. Santos <lycan.felipe@gmail.com>
2022-12-30 10:48:01 -0300

    Remove imports

--- back-end-crops/src/main/java/com/owl/systems/crops/repository/EventRepository.java
+++ back-end-crops/src/main/java/com/owl/systems/crops/repository/EventRepository.java
@@ -4,8 +4,6 @@ import com.owl.systems.crops.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
